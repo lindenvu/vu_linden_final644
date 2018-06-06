16be9392fca5e0df809e4e6f1762a3c8461f25bb
lindenvu <github@lindenvu.com>
2018-06-05 20:39:25 -0700

    Update 1.2

--- scripts/pizza.js
+++ scripts/pizza.js
@@ -1,11 +1,7 @@
+/*eslint-env browser*/
 var $ = function (id) {
     "use strict";
     return window.document.getElementById(id);
-};
-
-var msg = function () {
-    "use strict";
-    window.alert(this.id + " have been clicked");
 };
 
 /*
@@ -88,7 +84,7 @@ var step1FormJSON =
         },
         "state" :  {
             "formID" : "step1-state",
-            "invalidID" : "step1-name-invalid",
+            "invalidID" : "step1-state-invalid",
             "required" : "true",
             "checkForName" : "false",
             "checkForState" : "true",
@@ -102,46 +98,50 @@ var step1FormJSON =
             "required" : "true",
             "checkForName" : "false",
             "checkForState" : "false",
-            "checkForZip" : "false",
+            "checkForZip" : "true",
             "checkForTel" : "false",
             "checkForEmail" : "false"
         },
         "tel" :  {
             "formID" : "step1-tel",
-            "invalidID" : "step1-name-invalid",
+            "invalidID" : "step1-tel-invalid",
             "required" : "true",
             "checkForName" : "false",
             "checkForState" : "false",
             "checkForZip" : "false",
-            "checkForTel" : "false",
+            "checkForTel" : "true",
             "checkForEmail" : "false"
         },
         "email" :  {
-            "formID" : "step1-name",
-            "invalidID" : "step1-name-invalid",
+            "formID" : "step1-email",
+            "invalidID" : "step1-email-invalid",
             "required" : "true",
             "checkForName" : "false",
             "checkForState" : "false",
             "checkForZip" : "false",
             "checkForTel" : "false",
-            "checkForEmail" : "false"
+            "checkForEmail" : "true"
         }
     };
 
-window.alert(step1FormJSON.length);
-
 var step1Validated = true;
+
+window.alert(step1FormJSON[step1FormArray[7]].invalidID);
 
 var step1Validate = function () {
     "use strict";
+    window.alert("step1Validate function started");
     var x, y;
     for (x = 0; x < step1FormArray.length; x += 1) {
-        $(step1FormJSON[step1FormArray[x]].invalidID).className("invalid-feedback hide-me");
-        window.alert(step1FormJSON[step1FormArray[x]].invalidID);
+        //window.alert("classname loop started: " + step1FormJSON[step1FormArray[x]].invalidID);
+        $(step1FormJSON[step1FormArray[x]].invalidID).className = "invalid-feedback hide-me";
+        //window.alert("classname loop ended: " + step1FormJSON[step1FormArray[x]].invalidID);
     }
-     for (y = 0; y < step1FormArray.length; y += 1) {
+    window.alert("step1Validate function step 2 started");
+    for (y = 0; y < step1FormArray.length; y += 1) {
         if (step1FormJSON[step1FormArray[y]].required === "true") {
             if ($(step1FormJSON[step1FormArray[y]].formID).value === "") {
+                window.alert("required and empty: " + step1FormJSON[step1FormArray[y]].formID);
                 $(step1FormJSON[step1FormArray[y]].invalidID).classList.toggle("hide-me");
                 step1Validated = false;
             }
@@ -160,11 +160,13 @@ var step1Validate = function () {
             $("step1-tel").value + "\n" +
             $("step1-email").value + "\n"
     );
+    window.alert("step1Validate function finished");
     return step1Validated;
 };
 
 var step1Process = function () {
     "use strict";
+    window.alert("step1Process function exected");
     if (step1Validate()) {
         $("step1").innerHTML = "Return to Step 1: Enter Your Delivery Address";
         $("step1-form").classList.toggle("hide-me");
@@ -173,6 +175,8 @@ var step1Process = function () {
     } else {
         step1Validated = true;
     }
+    window.alert("");
+    window.alert("step1Process function finished");
     return false;
 };
 
@@ -183,20 +187,34 @@ var step2Validate = function () {
     return false;
 };
 
+var step2Process = function () {
+    "use strict";
+    step2Validate();
+    return false;
+};
+
 var step3Validate = function () {
     "use strict";
     return false;
 };
-    
+
+var step3Process = function () {
+    "use strict";
+    step3Validate();
+    return false;
+};
+
 var init = function () {
     "use strict";
+    window.alert("init function exected");
     $("step1-btn").onclick = step1Process;
-    $("step2-btn").onclick = msg;
-    $("step3-btn").onclick = msg;
+    $("step2-btn").onclick = step2Process;
+    $("step3-btn").onclick = step3Process;
     $("step2").classList.toggle("hide-me");
     $("step2-form").classList.toggle("hide-me");
     $("step3").classList.toggle("hide-me");
     $("step3-form").classList.toggle("hide-me");
+    window.alert("init function finished");
 };
 
 window.onload = init;
